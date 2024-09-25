bd305220082694d92adf0f131e3fb866c7075c18
unknown <gustavo_veras@ifsp.edu.br>
2024-09-25 12:10:33 -0300

    Endpoint GET /user criado

--- src/main/java/br/edu/ifsp/bra/proj_zero/controller/UserController.java
+++ src/main/java/br/edu/ifsp/bra/proj_zero/controller/UserController.java
@@ -1,8 +1,15 @@
 package br.edu.ifsp.bra.proj_zero.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.ifsp.bra.proj_zero.model.User;
+
 
 @RestController
 public class UserController {
-    
+    @GetMapping("/user")
+    public User getMethodName() {
+        return new User("Dev_Zero");
+    }
 }
