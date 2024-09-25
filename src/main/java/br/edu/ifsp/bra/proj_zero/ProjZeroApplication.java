92091ff1b24378d67408c72fbf86428effeeb99a
unknown <gustavo_veras@ifsp.edu.br>
2024-09-25 11:50:13 -0300

    Criação do projeto

---
+++ src/main/java/br/edu/ifsp/bra/proj_zero/ProjZeroApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.bra.proj_zero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjZeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsp/bra/proj_zero/ProjZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.bra.proj_zero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjZeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
