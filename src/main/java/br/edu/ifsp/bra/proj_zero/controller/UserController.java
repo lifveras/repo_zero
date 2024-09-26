22b3197f399e24b592c7fc9ebb17a2a6b658090e
Cailon83 <cailon.fonseca@aluno.ifsp.edu.br>
2024-09-26 16:06:59 -0300

    Adição GET /user-CAILON

--- src/main/java/br/edu/ifsp/bra/proj_zero/controller/UserController.java
+++ src/main/java/br/edu/ifsp/bra/proj_zero/controller/UserController.java
@@ -1,8 +1,16 @@
 package br.edu.ifsp.bra.proj_zero.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class UserController {
+    @GetMapping("/user-CAILON")
+    public String getMethodName() {
+        return "Olá Mundooo, sou o Cailonnn!!";
+    }
+    
     
 }
