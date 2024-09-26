4a2a6f35ab07ae8b5a6d33009df896c8aaaac58a
Professor <gustavo_veras@ifsp.edu.br>
2024-09-26 16:04:05 -0300

    Adição GET /user-prof

--- src/main/java/br/edu/ifsp/bra/proj_zero/controller/UserController.java
+++ src/main/java/br/edu/ifsp/bra/proj_zero/controller/UserController.java
@@ -1,8 +1,14 @@
 package br.edu.ifsp.bra.proj_zero.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class UserController {
-    
+    @GetMapping("/user-prof")
+    public String getMethodName() {
+        return "Olá mundo, sou o Prof. Luiz";
+    }    
 }
