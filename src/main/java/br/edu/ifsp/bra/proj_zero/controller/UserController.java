bdb0a7224082b0c5555b4df9d2717d4a4fd59923
unknown <gabriellybabi10@gmail.com>
2024-09-26 16:05:05 -0300

    Adição GET /user-gaby

--- src/main/java/br/edu/ifsp/bra/proj_zero/controller/UserController.java
+++ src/main/java/br/edu/ifsp/bra/proj_zero/controller/UserController.java
@@ -1,8 +1,15 @@
 package br.edu.ifsp.bra.proj_zero.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class UserController {
+    @GetMapping("/user-gaby")
+    public String getMethodName() {
+        return ("Olá mundo, eu sou a Gaby! ");
+    }
     
 }
